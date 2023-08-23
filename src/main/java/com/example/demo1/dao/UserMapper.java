188e233811356aa60d53c7af86c8174bffc4d275
windstar <2134182153@qq.com>
2023-08-23 23:58:20 +0800

    add login change1 demo->demo11

--- src/main/java/com/example/demo1/dao/UserMapper.java
+++ src/main/java/com/example/demo1/dao/UserMapper.java
@@ -4,4 +4,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     public userBean findbyUsername(String username);
+    public userBean addUser(String username);
 }
